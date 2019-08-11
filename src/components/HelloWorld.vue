26b4fcf1c85ff91b06ca794ef7181ebbe391be1f
Juozas Rastenis <juozas.rastenis@gmail.com>
2019-08-11 02:14:46 +0200

    initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,71 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h1>3D and Vue</h1>
+    <h3>Vue-babylonjs</h3>
+    <section>
+      <Scene @complete="sceneComplete">
+      <Camera type="arcRotate"></Camera>
+      <Entity>
+        <!-- <Animation property="rotation.z" :duration="15">
+          <Key frame="0%" :value="1"></Key>
+          <Key frame="50%" :value="3"></Key>
+          <Key frame="100%" :value="1"></Key>
+        </Animation> -->
+        <DirectionalLight specular="#fff" diffuse="#b3153e" :position="[-10, 15, -5]" :direction="[0,-1,-5]"></DirectionalLight>
+      </Entity>
+      <Entity :scaling="[1, 1, 1]">
+        <Ground :scaling="[35,1,15]" :rotation="[1.55,0,0]" :position="[0,1,-5]">
+          <Material emissive="#fff"></Material>
+        </Ground>
+        <Entity>
+          <Animation v-model="myModel" property="scaling.z" :duration="5">
+            <Key frame="0%" :value="1"></Key>
+          <Key frame="50%" :value="3"></Key>
+          <Key frame="100%" :value="1"></Key>
+          </Animation>
+          <Novicell></Novicell>
+        </Entity>
+      </Entity>
+    </Scene>
+    </section>
   </div>
+  
 </template>
 
 <script>
+import {BABYLON} from 'vue-babylonjs';
+import Novicell from './NovicellLogo.vue';
+
 export default {
-  name: 'HelloWorld',
+  components: {
+    Novicell
+  },
+  name: '',
+  data() {
+    return {
+      myModel: null
+    }
+  },
   props: {
     msg: String
+  },
+  mounted() {
+    console.log(BABYLON);
+    
+  },
+  watch: {
+    myModel(e) {
+      console.log(e);
+    }
+  },
+  methods: {
+    sceneComplete({scene}) {
+      console.log(scene);
+      // BABYLON.SceneLoader.Append("./", "novicell-logo.obj", scene, function (scene) {
+      //   // do something with the scene
+      //   console.log(scene);
+      // });
+    }
   }
 }
 </script>
---
+++ src/components/NovicellLogo.vue
@@ -0,0 +1,21 @@
+<template>
+    <Asset src="https://srv-file1.gofile.io/download/zbfO8Q/Nvcl-Logo.glb" :scaling="[50,50,50]" :position="[0, -1, 0]">
+    <!-- <Material diffuse="#008800"></Material> -->
+    <Material>
+      <Texture src="https://www.babylonjs-playground.com/textures/grass.png"></Texture>
+    </Material>
+    </Asset>
+</template>
+
+<script>
+
+export default {
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
